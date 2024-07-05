e1874e60117e14f11daaeaa70a7312a65f9ae4d1
ViniciusSenaCarmo <137804434+ViniciusSenaCarmo@users.noreply.github.com>
2024-07-05 13:24:46 +0000

    Endpoint collection integrado ao banco de dados

--- index.js
+++ index.js
@@ -10,4 +10,29 @@ app.get('/oi', function (req, res) {
     res.send('Olá, mundo!')
   })
   
-app.listen(3000)
+  // Lista de personagens
+  const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+  //Red All - [GeT] /item
+  app.get ('/item', function (req, res) {
+    //pegamos a lista e enviamos como resposta HTTP
+    res.send(lista)
+  })
+
+  
+  //sinalizamos para o Express que vamos usar JSON no Body
+  app.use(express.json())
+
+  //Create - [Post] /item
+  app.post('/item', function (req, res) {
+    //obtemos o nome enviado no Request Body
+    const item = req.body.nome
+    
+    //Inserimos o item no final da lista
+    lista.push(item)
+    
+    //Enviamos uma mensagem de sucesso!
+    res.send('Item criado com sicesso!')
+  })
+
+  app.listen(3000)
